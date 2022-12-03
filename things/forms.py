7a8beadf39b4a09f59fc02ffe1abb6d8785f0abc
Dion Munipi <dion.munipi@kcl.ac.uk>
2022-12-03 16:39:39 +0000

    .

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
-
+from django import forms
+from .models import Thing
 # Create your forms here.
+
+
+class ThingForm(forms.Form):
+    name = forms.CharField(label="name")
+    description = forms.TextArea(label="description")
+    quantity = forms.NumberInput(label="quantity")
