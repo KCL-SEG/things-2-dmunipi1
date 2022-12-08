711766196c064d4005d3ba31383a8cd2818971ca
Dion Munipi <dion.munipi@kcl.ac.uk>
2022-12-08 17:41:59 +0000

    Fix errors

--- things/forms.py
+++ things/forms.py
@@ -1,10 +1,11 @@
 """Forms of the project."""
 from django import forms
 from .models import Thing
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your forms here.
 
 
 class ThingForm(forms.Form):
-    name = forms.CharField(label="Name")
-    description = forms.CharField(widget=forms.TextInput(attrs={'name':'description'}))
-    quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'name':'quantity'}))
+    name = forms.CharField(label="Name", max_length=35)
+    description = forms.CharField(widget=forms.Textarea(attrs={'name':'description'}), max_length=120)
+    quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'name':'quantity'}), validators=[MinValueValidator(0),MaxValueValidator(50)])
